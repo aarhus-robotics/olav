b7e61e11b916853e3487faea097af275e1e087e5
taskbjorn <36376776+taskbjorn@users.noreply.github.com>
2025-01-07 14:28:44 +0100

    Refactor gamepad node file tree

--- olav_peripherals/src/gamepad/executor.cpp
+++ olav_peripherals/src/gamepad/executor.cpp
@@ -30,7 +30,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include <olav_peripherals/input/gamepad_interface_node.hpp>
+#include <olav_peripherals/gamepad/node.hpp>
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
